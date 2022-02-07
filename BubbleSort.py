223510351a4fada96303e9dbc39925d35a283d44
Pallavi-medidi <91972938+Pallavi-medidi@users.noreply.github.com>
2022-02-07 13:29:06 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,2 +1,14 @@
 def Bubblesort(list) :
+   for i in range(len(list)-1, 0, -1) :
+      for j in range(i) :
+         if list[j] > list[j + 1] :
+            temp = list[j]
+            list[j] = list[j + 1]
+            list[j + 1] = temp
+   return list
+
+l = [5, 7, 8,4, 3,1, 2,90]
+print(Bubblesort(l))
+
+            
    
